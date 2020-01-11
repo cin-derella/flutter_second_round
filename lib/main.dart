5b11518fa82480b5dbd4716ba4f4ee4e3cd1478c
cin-derella <shirley_cinderella@outlook.com>
2020-01-11 21:36:56 +0800

    split app into widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import './question.dart';
-import './answer.dart';
+import 'package:flutter_second_round/result.dart';
+import './quiz.dart';
+import './result.dart';
 
 // void main(){
 //   runApp(MyApp());
@@ -20,7 +21,7 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
 //_MyAppState is a syntax that turns public class to a private class
 
-  final questions = const [
+  final _questions = const [
     {
       'questionText': 'What is your favorite color?',
       'answers': ['Black', 'Red', 'Green', 'White']
@@ -45,7 +46,7 @@ class _MyAppState extends State<MyApp> {
 //setState is a function that forces Flutter to re-render the user interface,but not the entire user interface
 //it updates widget by calling its build method where it calls setState
 
-    if (_questionIndex < questions.length) {
+    if (_questionIndex < _questions.length) {
       print('We have more questions!');
     } else {
       print('No more questions!');
@@ -59,41 +60,13 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: Text('My Second Flutter App'),
         ),
-        body: _questionIndex < questions.length
-            ? Column(
-                children: <Widget>[
-                  Question(
-                    questions[_questionIndex]['questionText'],
-                  ),
-                  ...(questions[_questionIndex]['answers'] as List<String>)
-                      .map((answer) {
-                    return Answer(_answerQuestion, answer);
-                  }).toList(),
-                  // RaisedButton(
-                  //   child: Text('Answers 1'),
-                  //   onPressed: _answerQuestion,
-                  //   //onPressed requires a function, pass the name of the function,not the result of the function to onPressed: answerQuestion(),
-                  //   //so it wont be executed when the code was parsed from top to bottom
-                  // ),
-                  // RaisedButton(
-                  //   child: Text('Answers 2'),
-                  //   onPressed: () => print('Answer 2 chosen.'),
-                  // ),
-                  // // the function is a anonymous function that cant be called from outside, it'll be executed only once.
-                  // //and also it is a function, not the return value of the function and would not execute immediately.
-
-                  // RaisedButton(
-                  //   child: Text('Answers 3'),
-                  //   onPressed: () {
-                  //     //...
-                  //     print('and then answer 3 chosen');
-                  //   },
-                  // ),
-                ],
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                answerQuestion: _answerQuestion,
+                questionIndex: _questionIndex,
+                questions: _questions,
               )
-            : Center(
-                child: Text('You did it!'),
-              ),
+            : Result(),
       ),
     );
   }
---
+++ lib/quiz.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import './question.dart';
+import './answer.dart';
+
+class Quiz extends StatelessWidget {
+  final List<Map<String, Object>> questions;
+  final int questionIndex;
+  final Function answerQuestion;
+
+  Quiz({
+    @required this.questions,
+    @required this.answerQuestion,
+    @required this.questionIndex,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Question(
+          questions[questionIndex]['questionText'],
+        ),
+        ...(questions[questionIndex]['answers'] as List<String>).map((answer) {
+          return Answer(answerQuestion, answer);
+        }).toList(),
+        // RaisedButton(
+        //   child: Text('Answers 1'),
+        //   onPressed: _answerQuestion,
+        //   //onPressed requires a function, pass the name of the function,not the result of the function to onPressed: answerQuestion(),
+        //   //so it wont be executed when the code was parsed from top to bottom
+        // ),
+        // RaisedButton(
+        //   child: Text('Answers 2'),
+        //   onPressed: () => print('Answer 2 chosen.'),
+        // ),
+        // // the function is a anonymous function that cant be called from outside, it'll be executed only once.
+        // //and also it is a function, not the return value of the function and would not execute immediately.
+
+        // RaisedButton(
+        //   child: Text('Answers 3'),
+        //   onPressed: () {
+        //     //...
+        //     print('and then answer 3 chosen');
+        //   },
+        // ),
+      ],
+    );
+  }
+}
