89fde60a3d4c99c05cb4cf913d15574a20445c31
cin-derella <shirley_cinderella@outlook.com>
2020-01-11 19:59:28 +0800

    map lists to widget

--- lib/main.dart
+++ lib/main.dart
@@ -13,20 +13,18 @@ void main() => runApp(MyApp());
 class MyApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-
     return _MyAppState();
   }
 }
 
-
-class _MyAppState extends State <MyApp>{
+class _MyAppState extends State<MyApp> {
 //_MyAppState is a syntax that turns public class to a private class
 
   var _questionIndex = 0;
 
   void _answerQuestion() {
     setState(() {
-          _questionIndex = _questionIndex + 1;
+      _questionIndex = _questionIndex + 1;
     });
 //setState is a function that forces Flutter to re-render the user interface,but not the entire user interface
 //it updates widget by calling its build method where it calls setState
@@ -36,9 +34,20 @@ class _MyAppState extends State <MyApp>{
 
   @override
   Widget build(BuildContext context) {
+     
     var questions = [
-      'What is your favorite color?',
-      'What is your favorite animal?'
+      {
+        'questionText': 'What is your favorite color?',
+        'answers': ['Black', 'Red', 'Green', 'White']
+      },
+      {
+        'questionText': 'What is your favorite animal?',
+        'answers': ['Cat', 'Dog', 'Rabbit', 'Lion']
+      },
+      {
+        'questionText': 'Who is your favorite singer?',
+        'answers': ['Adele', 'Rihanna', 'Eminem', 'Lady Gaga']
+      },
     ];
 
     return MaterialApp(
@@ -49,11 +58,12 @@ class _MyAppState extends State <MyApp>{
         body: Column(
           children: <Widget>[
             Question(
-              questions[_questionIndex],
+              questions[_questionIndex]['questionText'],
             ),
-            Answer(_answerQuestion),
-            Answer(_answerQuestion),
-            Answer(_answerQuestion),
+            ...(questions[_questionIndex]['answers']as List<String>).map((answer){
+              return Answer(_answerQuestion,answer);
+
+            }).toList(),
             // RaisedButton(
             //   child: Text('Answers 1'),
             //   onPressed: _answerQuestion,
