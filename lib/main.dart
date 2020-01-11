d8642d6ab28644e8b5165348d8663c62e2090cfb
cin-derella <shirley_cinderella@outlook.com>
2020-01-11 13:08:18 +0800

    connect functions and buttons

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,11 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+
+  void answerQuestion(){
+    print('Answer chosen!');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -16,7 +21,26 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('My Second Flutter App'),
         ),
-        body: Text('This is the default text!!'),
+        body:Column(
+          children: <Widget>[
+            Text('The question!'),
+            RaisedButton(
+              child: Text('Answers 1'),
+              onPressed: answerQuestion,
+              //onPressed requires a function, pass the name of the function,not the result of the function to onPressed: answerQuestion(), 
+              //so it wont be executed when the code was parsed from top to bottom
+        
+            ),
+            RaisedButton(
+              child: Text('Answers 2'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answers 3'),
+              onPressed: answerQuestion,
+            ),
+          ],
+        ),
       ),
     );
   }
