913ac7adde4f124f2e556c1cccb67d2b98876886
cin-derella <shirley_cinderella@outlook.com>
2020-01-11 12:25:10 +0800

    add visible widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/material.dart';
 
+// void main(){
+//   runApp(MyApp());
+// }
 
-void main(){
-  runApp(MyApp());
-}
+//shorthand notion for function that only have one and exactly one expression  (below)
 
-class MyApp extends StatelessWidget{
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
-    return MaterialApp(home:Text('Hello'),);
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My Second Flutter App'),
+        ),
+        body: Text('This is the default text!!'),
+      ),
+    );
   }
 }
