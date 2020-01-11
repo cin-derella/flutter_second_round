cefa3e66bb16139ed68400b48af681ff053a0a19
cin-derella <shirley_cinderella@outlook.com>
2020-01-11 17:47:14 +0800

    first style and layout

--- lib/question.dart
+++ lib/question.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
 
 class Question extends StatelessWidget {
-  final String  questionText ;
+  final String questionText;
   //final tells Dart that this value will never change after its initialization
   Question(this.questionText);
 
   @override
   Widget build(BuildContext context) {
-    return Text(questionText);
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.all(10),
+      child: Text(
+        questionText,
+        style: TextStyle(fontSize: 28),
+        textAlign: TextAlign.center,
+      ),
+    );
   }
 }
