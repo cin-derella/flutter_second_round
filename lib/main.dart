c970478196d8f3af031df982c7d6039fa3701b87
cin-derella <shirley_cinderella@outlook.com>
2020-01-11 22:36:40 +0800

    reset

--- lib/main.dart
+++ lib/main.dart
@@ -38,7 +38,6 @@ class _MyAppState extends State<MyApp> {
         {'text': 'Snake', 'score': 11},
         {'text': 'Elephant', 'score': 5},
         {'text': 'Lion', 'score': 9},
-
       ]
     },
     {
@@ -54,6 +53,13 @@ class _MyAppState extends State<MyApp> {
 
   var _questionIndex = 0;
   var _totalScore = 0;
+
+  void _resetQuiz() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
 
   void _answerQuestion(int score) {
     _totalScore += score;
@@ -85,7 +91,7 @@ class _MyAppState extends State<MyApp> {
                 questionIndex: _questionIndex,
                 questions: _questions,
               )
-            : Result(_totalScore),
+            : Result(_totalScore,_resetQuiz),
       ),
     );
   }
